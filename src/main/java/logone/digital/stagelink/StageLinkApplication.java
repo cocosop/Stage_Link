5f08da024666939ff1343535d26ad3787ef6a576
cocosop <corinemanuella@yahoo.fr>
2023-06-26 19:16:29 +0100

    initialisation

---
+++ src/main/java/logone/digital/stagelink/StageLinkApplication.java
@@ -0,0 +1,13 @@
+package logone.digital.stagelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/logone/digital/stagelink/StageLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package logone.digital.stagelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
