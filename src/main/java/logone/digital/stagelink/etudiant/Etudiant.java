11401841e44f4785c07ce3fa836541c2ab1531b9
mintoua <toupkandi.mintoua@esprit.tn>
2023-07-10 21:26:31 +0100

    added etudiant package et created Etudiant class entity

---
+++ src/main/java/logone/digital/stagelink/etudiant/Etudiant.java
@@ -0,0 +1,29 @@
+package logone.digital.stagelink.etudiant;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_etudiant")
+public class Etudiant implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nom;
+    private String prenom;
+    private String matricule;
+
+}
