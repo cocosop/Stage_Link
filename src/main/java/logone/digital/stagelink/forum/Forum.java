6575cad20c60a99238146e1f5add19c5d8c62cca
Toureyakhouba <yakhoubatoure15@gmail.com>
2023-07-10 21:52:18 +0300

    j'ai cree une branche toure

--- src/main/java/logone/digital/stagelink/forum/Forum.java
+++ src/main/java/logone/digital/stagelink/forum/Forum.java
@@ -26,7 +26,6 @@ public class Forum implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String publication;
-    // cette annotaion jsonIgnore veut dire que cette relation doit dire ignore
     @JsonIgnore
     @OneToMany(mappedBy = "forum",cascade = CascadeType.ALL)
     private List<Commentaire> commentaires;
