0a6a1f421e037affe57876ecf3d57fb7d4039ece
cocosop <corinemanuella@yahoo.fr>
2023-07-11 15:08:47 +0100

    crud des classes etudiant et entreprise

--- src/main/java/logone/digital/stagelink/etudiant/EtudiantEntity.java
+++ src/main/java/logone/digital/stagelink/etudiant/EtudiantEntity.java
@@ -1,2 +1,49 @@
-package logone.digital.stagelink.etudiant;public class EtudiantEntity {
+package logone.digital.stagelink.etudiant;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "_etudiant")
+@Builder
+public class EtudiantEntity {
+
+
+    @Serial
+    private static final  long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    @Column
+    private String titre;
+
+    @Column
+    private String description;
+
+    @Column
+    private String entreprise;
+
+    @Column
+    private String domaine;
+
+    @Column
+    private String localisation;
+
+    @Column
+    private Instant dateDebut;
+
+    @Column
+    private Instant dateFin;
+
+
+
 }
